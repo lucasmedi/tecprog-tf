c6b72abde6293bdacb7ee3216bdedba2185e5726
giovanni.bat@gmail.com <giovanni.bat@gmail.com@341adb1f-aa60-65a5-fbbd-0f0d0bec90e9>
2013-07-02 06:06:14 +0000

    alterações nas telas

--- src/view/AutorView.java
+++ src/view/AutorView.java
@@ -1,40 +1,61 @@
 package view;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
+import exceptions.BusinessException;
+import exceptions.ConnectionException;
 import business.bo.Autor;
+import business.bo.Editora;
+import business.domain.AutorRepository;
+import business.domain.EditoraRepository;
 
 @ManagedBean(name="autorView")
 @SessionScoped
 public class AutorView {
 
 	private Autor autor;
-	private DataModel<Autor> autorDataModel;
+	private DataModel<Autor> autores;
+	private AutorRepository autorRepository;
 	
 	@PostConstruct
-	public void init(){
+	public void init() {
 		autor = new Autor();
+	    try {
+	    	autorRepository = new AutorRepository();
+		} catch (BusinessException | ConnectionException e) {
+			// Mostrar mensagem de erro.
+		}
 	}
 	 
 	public String cadastrarAutor(){
 		return "autor";
 	}
-	public String pesquisarAutor(){
-		return "autor";
+	
+	public void pesquisarAutor() {
+		try {
+			autores = new ListDataModel<>(autorRepository.buscarTodos());
+			//editoraDataModel = new ListDataModel<>(list);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
 	}
-	public DataModel<Autor> getListarAutor(){
-		return getAutorDataModel();
-	}
+	
 	public String preparaAlterarAutor(){
 		return "autor";
 	}
+	
 	public String excluirAutor(){
 		return "autor";
 	}
 	
+	
+	//GETs and SETs
 	public Autor getAutor() {
 		return autor;
 	}
@@ -42,14 +63,18 @@ public class AutorView {
 	public void setAutor(Autor autor) {
 		this.autor = autor;
 	}
-	
-	public DataModel<Autor> getAutorDataModel() {
-		return autorDataModel;
+
+	public DataModel<Autor> getAutores() {
+		return autores;
 	}
-	
-	public void setAutorDataModel(DataModel<Autor> autorDataModel) {
-		this.autorDataModel = autorDataModel;
+
+	public void setAutores(DataModel<Autor> autores) {
+		this.autores = autores;
 	}
+
+	
+	
+	
 	
 	
 }
